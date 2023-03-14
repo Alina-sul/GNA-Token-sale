bfd4d1eba30c326c4fa6d45cb8a8e2cdcc5a884f
Alina Sulkovsky <alina@findings.co>
2023-03-14 10:15:45 +0200

    .

--- truffle/contracts/GNAToken.sol
+++ truffle/contracts/GNAToken.sol
@@ -7,5 +7,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract GNAToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("Gil and Alina Token", "GNA") {
         _mint(msg.sender, initialSupply);
+
     }
 }
