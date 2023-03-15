758a0849b49f026290df2a5c8fd3286f2b79669a
Alina Sulkovsky <alina@findings.co>
2023-03-15 20:46:00 +0200

    added send token test

--- truffle/test/GNAToken.test.js
+++ truffle/test/GNAToken.test.js
@@ -12,11 +12,23 @@ const expect = chai.expect;
 
 contract("Token test", async accounts => {
 
+    const [deployerAccount, recipient, anotherAccount] = accounts;
+
     it("all tokens should be in my account", async () => {
         let instance = await Token.deployed();
         let totalSupply = await instance.totalSupply();
-        let balance = await instance.balanceOf(accounts[0]);
+        let balance = await instance.balanceOf(deployerAccount);
         expect(balance).to.be.a.bignumber.that.equals(totalSupply);
     } );
+
+    it("is possible to send tokens between accounts", async () => {
+        const sendTokens = 1;
+        let instance = await Token.deployed();
+        let totalSupply = await instance.totalSupply();
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.that.equals(totalSupply);
+        expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.that.equals(totalSupply.sub(new BN(sendTokens)));
+        expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.that.equals(new BN(sendTokens));
+    });
 
 });
