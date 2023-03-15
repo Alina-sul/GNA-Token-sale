371b53b42d7559bd54a8cc5326956c438f562bc9
Alina Sulkovsky <alina@findings.co>
2023-03-15 21:19:00 +0200

    test

--- truffle/test/GNAToken.test.js
+++ truffle/test/GNAToken.test.js
@@ -31,4 +31,12 @@ contract("Token test", async accounts => {
         expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.that.equals(new BN(sendTokens));
     });
 
+    // it("is not possible to send more tokens than account 1 has", async () => {
+    //     let instance = await Token.deployed();
+    //     let balanceOfAccount = await instance.balanceOf(deployerAccount);
+    //     expect(instance.transfer(recipient, new BN(balanceOfAccount+1))).to.eventually.be.rejected;
+    //     //Check if the balance is still the same
+    //     expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.that.equals(balanceOfAccount);
+    // });
+
 });
